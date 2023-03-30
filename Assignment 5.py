a4a9953bbf92a038fc390e589a541ce7584ded49
Ricky Cahyadi <rickycahyadi24@gmail.com>
2023-03-31 00:52:14 +0800

    Assignment 5

---
+++ Assignment 5.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# 1. Input
+raw_data = pd.read_csv("Menu.csv")
+print(raw_data)
+print(raw_data.info())
+
+# 2. Process
+raw_data.sort_values(["Menu"],axis=0,inplace=True)
+
+# 3. Output
+print(raw_data)
